d3790a4aeb089c0f29c43772baec1791121b4464
Olexiy Likhosherstov <alikhosherstov@gmail.com>
2023-05-10 15:41:06 +0300

    Add main.go

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/olikhosherstov/examples_gin_postgresql_crud/pkg/common/books"
+	"github.com/olikhosherstov/examples_gin_postgresql_crud/pkg/common/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("../pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+	h := db.Init(dbUrl)
+
+	books.RegisterRoutes(r, h)
+
+	r.Run(port)
+}
