6cc42e0883f8460803480f1b90f938cc54d51439
Olexiy Likhosherstov <alikhosherstov@gmail.com>
2023-05-11 10:43:45 +0300

    Updates

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,15 @@ import (
 	"github.com/olikhosherstov/examples_gin_postgresql_crud/pkg/common/db"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigFile("../pkg/common/envs/.env")
-	viper.ReadInConfig()
-
-	port := viper.Get("PORT").(string)
-	dbUrl := viper.Get("DB_URL").(string)
+	dbUrl := "postgres://goapimedium:wsadqe123@localhost:5432/go_medium_api"
 
 	r := gin.Default()
 	h := db.Init(dbUrl)
 
 	books.RegisterRoutes(r, h)
 
-	r.Run(port)
+	r.Run()
 }
